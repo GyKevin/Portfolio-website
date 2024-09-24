347a7b5eb109882a1e8ec788b5d9b1407bfcd5ec
Kevin <kvindso@gmail.com>
2024-09-24 10:53:51 +0200

    lightmode works now
    yipeeeee

--- lightmode.js
+++ lightmode.js
@@ -1,5 +1,18 @@
+const themeStorage = localStorage.getItem('themeStorage');
 const lightModeToggle = document.querySelector('#lightModeSwitch');
 
+if (themeStorage) {
+    document.documentElement.setAttribute('data-theme', themeStorage);
+}
+
+//on button click, give the button an event listener 
 lightModeToggle.addEventListener('click', () => {
-    
+    const theme = document.documentElement.getAttribute('data-theme');
+
+    // check if the theme is dark or light and then change the data-theme
+    const newTheme = theme === 'dark' ? 'light' : 'dark';
+    document.documentElement.setAttribute('data-theme', newTheme);
+
+    // save theme to localStorage
+    localStorage.setItem('themeStorage', newTheme);
 });
