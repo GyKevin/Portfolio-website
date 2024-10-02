ed589b885c003824997fd3691f854a81802dc53c
Kevin <kvindso@gmail.com>
2024-10-02 10:03:38 +0200

    uh i think it works?
    the root folder be broken tho

---
+++ controllers/pageController.php
@@ -0,0 +1,6 @@
+<?php
+class pageController {
+    public function home() {
+        require './views/home/home.view.php';
+    }
+}
--- index.php
+++ index.php
@@ -1,2 +1,33 @@
 <?php
-require './views/home/home.view.php';
+// Autoload controllers and models
+spl_autoload_register(function($className) {
+    if (file_exists("./controllers/" . $className . ".php")) {
+        require_once "./controllers/" . $className . ".php";
+    } elseif (file_exists("./models/" . $className . ".php")) {
+        require_once "./models/" . $className . ".php";
+    }
+});
+
+// Remove query strings if any
+$request = strtok($_SERVER['REQUEST_URI'], '?');
+
+// Set the default controller to PagesController
+$controller = new PageController();
+
+// Simple routing based on URL
+switch ($request) {
+    case '/':
+        $controller->home();
+        break;
+    case '/portfolio':
+        $controller->portfolio();
+        break;
+    case '/about':
+        $controller->about();
+        break;
+    case '/contact':
+        $controller->contact();
+        break;
+    default:
+        echo '404 Page Not Found';
+}
