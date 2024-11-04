c846efdc3efe12165a99e29b8440593b433f3909
Kevin <kvindso@gmail.com>
2024-11-04 11:20:35 +0100

    concatenate fix

--- public/concatenate.js
+++ public/concatenate.js
@@ -1,15 +1,16 @@
-var paragraph = document.querySelector('.project-txt-cont p');
-var paragraphText = paragraph.textContent;
-// console.log(paragraph.textContent);
-// voeg ... aan het einde van de text toe
-var concate = " ...";
-
-// als de tekst langer is dan 100 karakters...
-if (paragraphText.length > 100) {
-    // slice de rest van de tekst en zet ... aan het einde
-    var newText = paragraphText.slice(0, 100) + concate;
-
-    paragraphText = newText;
-    paragraph.innerHTML = paragraphText;
-    // console.log(paragraphText);
-}
+var paragraph = document.querySelectorAll('.project-txt-cont p');
+// voeg ... toe aan elk project, niet alleen het eerste
+paragraph.forEach(function(paragraph) { 
+    var paragraphText = paragraph.textContent;
+    // voeg ... aan het einde van de text toe
+    var concate = " ...";
+    
+    // als de tekst langer is dan 100 karakters...
+    if (paragraphText.length > 100) {
+        // slice de rest van de tekst en zet ... aan het einde
+        var newText = paragraphText.slice(0, 100) + concate;
+    
+        paragraphText = newText;
+        paragraph.innerHTML = paragraphText;
+    }
+});
